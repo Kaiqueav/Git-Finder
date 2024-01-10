50019bc705629bcc51136b1672c967b07556d982
Kaiqueav <kaiquecerq66@outlook.com>
2024-01-10 19:32:13 -0300

    estruturando pastas

---
+++ src/components/ListRepo/ListRepo.jsx
@@ -0,0 +1,7 @@
+export const ListRepo = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,17 @@
+import { Header } from "../components/Header/Header"
+import { ListRepo } from "../components/ListRepo/ListRepo"
+import { Profile } from "../components/Profile/Profile"
+import { Search } from "../components/Search/Search"
+ 
+
+export const Page = () =>{
+    return (
+        <>
+        <Header/> 
+        <ListRepo/>
+        <Profile/> 
+        <Search/> 
+        </>
+
+    )
+}
