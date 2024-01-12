d37f3903f34e4dd9ca2e943c42af91d523838e8f
Kaiqueav <kaiquecerq66@outlook.com>
2024-01-11 22:39:44 -0300

    componete profile e search

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,19 @@
 export const Profile = () => {
     return(
-        <div></div>
+        <div className=" profile   flex p-2 text-white mt-3" >
+                <div>
+                    <img src="https://avatars.githubusercontent.com/u/132000864?s=400&u=d53c75a22a14a1f27d777652beedd47a3350736e&v=4"  
+                    alt="" className="w-20 rounded-full ml-14 border-2 border-white"  />
+                    </div>
+                <div className="ml-12 font-semibold p-4 ">
+                    <h1 className="ml-3"> Kaiqueav</h1>
+                    <h3> @kaiqueav</h3>
+                    <p className="text-[#9999] p-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Ratione maxime ea vitae officia doloremque.</p>
+                    <h1 className="font-bold text-2xl ml-0 mt-4">Repositorios</h1>
+                </div>
+               
+        </div>
+
     )
 }
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,10 @@
 export const Search = () => {
     return (
-        <div>
+        <div className="p-1 ml-7">
+            <input placeholder="@userName" type="text" className="bg-slate-800 rounded-md p-1" />
+            <button className="bg-slate-800 text-white font-bold ml-8 p-1 w-[98px] rounded-md  ">
+                Buscar
+            </button>
         </div>
     )
 }
